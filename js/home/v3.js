34f2625ecc335a80668d5f81e19af89d47ea1e80
caibhh <caibhh@caib.ml>
2021-05-20 14:53:47 +0800

    update

--- js/home/v3.js
+++
@@ -1,6 +0,0 @@
-var windowWidth = $(window).width();
-if (windowWidth < 767) {
-    // do something
-    $("#aplayer").remove();
-}
-var meting_api = 'https://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id';
